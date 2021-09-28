27541e742158fcde8ed38950b447ffe665e18a5b
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-09-27 23:16:06 -0400

    Create fun_new_init.py

---
+++ Fun_Stuff/fun_new_init.py
@@ -0,0 +1,15 @@
+# Topic: The relations between two dunder methods
+# __new__ vs. __init__
+# File: fun_new_init.py
+#
+# Author: Xuhua Huang
+# Last updated: Sept 27, 2021
+# Created on: Sept 27, 2021
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
