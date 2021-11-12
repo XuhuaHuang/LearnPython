2468f39999d2e1eeec37d286c3bb9d9b96a8cc8a
XuhuaHuang <xuhuahuang0412@gmail.com>
2021-11-12 11:16:45 -0500

    Added comment to identify the usage.

--- Util/lookupvalue.py
+++ Util/lookupvalue.py
@@ -4,8 +4,11 @@
 # Last updated: Oct 20, 2021
 # Created on: Oct 20, 2021
 
-from typing import Any
+from typing import Any, Optional
 
 
-def lookup_by_value(_dict: dict, value: Any):
+def lookup_by_value(_dict: dict, value: Any) -> Optional[Any]:
+    # Using the .keys() method to collect all dictionary keys and convert to a list
+    # then use the [] operator on the constructed list
+    # with the .index() method on all colected dictionary values to find key value
     return list(_dict.keys())[list(_dict.values()).index(value)]
